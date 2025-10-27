edbfb496c55c546ed2fe190a3519f401d87ca559
ThinhLe <ThinhLe_ctr@fortna.com>
2025-10-27 14:35:52 +0700

    update

--- Day-7-ThreadSynchronized-Volatile/src/Main.java
+++ Day-7-ThreadSynchronized-Volatile/src/Main.java
@@ -1,13 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) throws InterruptedException {
         System.out.println("-----------RaceCondition Problem Start-----------------");
-        /** Khi nhiều luồng cùng update cùng 1 thuộc tính sẽ xảy ra lỗi Race Condition, dữ liệu không được đảm bảo */
+        /** Khi nhiều luồng cùng update cùng 1 biến sẽ xảy ra lỗi Race Condition, dữ liệu không được đảm bảo */
         new SynchronizedSolution().raceConditionProblem();
 
         System.out.println("-----------Synchronized Solution Start-----------------");
-        /**  Khi nhiều luồng cùng update cùng 1 thuộc tính đồng bộ dùng Synchronized, kết quả sẽ đúng nhưng vì lock nên sẽ dính performance */
+        /**  Khi nhiều luồng cùng update cùng 1 thuộc tính đồng bộ dùng Synchronized, kết quả sẽ đúng nhưng vì lock nên time tăng cao */
         new SynchronizedSolution().synchronizedSolution();
 
         System.out.println("-----------Visibility Problem Start-----------------");
@@ -16,11 +15,12 @@ public class Main {
 
         System.out.println("-----------Volatile Solution Start-----------------");
         /**
-         * Volatile chỉ đảm bảo kết quả được đẩy xuống ram luôn để các luồng khác đọc được kết quả mới nhất,
-         * không đảm bảo cho cập nhật nhiều luồng cùng lúc,
+         * Volatile đảm bảo kết quả được đẩy xuống ram luôn để các luồng khác đọc được kết quả mới nhất, không đảm bảo cho cập nhật nhiều luồng cùng lúc,
          * điều này giúp tránh performance với solution là chỉ duy nhất 1 luồng ghi và các luồng khác luôn đọc được kết quả mới nhất
          * --> Hậu quả: đọc chậm hơn nhiều so với đọc từ cache CPU thông thường vì khi dùng volatile thì luôn đọc từ RAM.
-         * --> Nhưng so với dùng Synchronized, chi phí này là rất nhỏ so với lợi ích đảm bảo đúng hành vi đa luồng, và Synchronized có thể gây dead block
+         * --> Nhưng so với dùng Synchronized, chi phí này là rất nhỏ so với lợi ích đảm bảo đúng hành vi đa luồng, ngoài ra Synchronized có thể gây deadlock
+         * --> Lưu ý: chỉ dùng volatile với dữ liệu nguyên thủy và object immutable (bất biến) như String, vì khi gán giá trị mới thì String gán vào địa chỉ ô nhớ mới chứ không thay đổi dữ liệu cũ
+         *     Với các object như List<> Map<> thì volatile chỉ đảm bảo luôn đọc địa chỉ ô nhớ mới nhất, chứ không đảm bảo dữ liệu ở ô nhớ đó
          */
         new VolatileSolution().volatileSolution();
     }
